3f7d47239012fe28ff27c75da4193639bd1ea63e
SDSD STAFF <SDSD STAFF@DESKTOP-3L41P6T>
2023-12-12 11:58:26 +0100

    Add project files.

---
+++ TicketsAPI/Core/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketsAPI.Core.Entities;
+
+namespace TicketsAPI.Core.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ TicketsAPI/Core/Entities/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketsAPI.Core.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime Time { get; set; }
+        public string PassengerName { get; set; }
+        public long PassengerSSN { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime CreatedAt { get; set; }= DateTime.Now;
+        public DateTime UpdatedAt { get; set; }=DateTime.Now;
+        public string ConfidentialComment { get; set; } = "Normal";
+
+
+    }
+}
